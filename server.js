a59cf6059817c0b139ef832889c7af0a962effab
Murilo Sousa Moraes <murilosousamoraes@gmail.com>
2022-11-02 16:20:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src/'));
+app.use(express.static('../shawandpartners/'));
 
 app.get('/*', (req, res) =>
     res.sendFile('./src/index.html'),
