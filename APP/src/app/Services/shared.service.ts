8fe4f9e4224e6292ffe6aa88ca2e1aa34265de5c
Murilo Sousa Moraes <murilosousamoraes@gmail.com>
2022-11-02 12:43:28 -0300

    refactor: Deploying the api and getting new api urls

--- APP/src/app/Services/shared.service.ts
+++ APP/src/app/Services/shared.service.ts
@@ -11,16 +11,16 @@ export class SharedService {
 
     GetAllUsers(id: string) {
         const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this._httpClient.get("http://localhost:5000/api/Users/GetAllUsers/" + id, {headers});
+        return this._httpClient.get("https://shawandpartners-murilo.herokuapp.com/api/Users/GetAllUsers/" + id, {headers});
     }
 
     GetUserDetails(login: string) {
         const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this._httpClient.get("http://localhost:5000/api/Users/GetUserDetails/" + login, {headers});
+        return this._httpClient.get("https://shawandpartners-murilo.herokuapp.com/api/Users/GetUserDetails/" + login, {headers});
     }
 
     GetUserRepos(login: string) {
         const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this._httpClient.get("http://localhost:5000/api/Users/GetUserRepos/" + login, {headers});
+        return this._httpClient.get("https://shawandpartners-murilo.herokuapp.com/api/Users/GetUserRepos/" + login, {headers});
     }
 }
